957267bd81373d5c210bffedff4d13e55069a856
IvanKolev12 <vanko200215@gmail.com>
2019-04-20 11:24:41 +0300

    New files

---
+++ Sorting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] arr = {'a','t','b','z','s','q','l','c','h'};
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                for (int j = 0; j < arr.Length-1; j++)
+                {
+                    if (arr[j]>arr[j+1])
+                    {
+                        char tempVar = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = tempVar;
+                    }
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]+" ");
+            }
+        }
+    }
+}
